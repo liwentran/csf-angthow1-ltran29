5eb5b912590c02185f09503e58056b3f968cbf39
Sky <skyngthowhing@gmail.com>
2022-09-19 20:02:41 -0400

    implemented draw pixel

--- csf_assign02/c_drawing_funcs.c
+++ csf_assign02/c_drawing_funcs.c
@@ -146,6 +146,11 @@ int64_t square_dist(int64_t x1, int64_t y1, int64_t x2, int64_t y2){
 //
 void draw_pixel(struct Image *img, int32_t x, int32_t y, uint32_t color) {
   // TODO: implement
+  if(in_bounds(img, x, y)){
+    set_pixel(img, compute_index(img, x, y), color);
+  }
+
+  return img;
   
 }
 

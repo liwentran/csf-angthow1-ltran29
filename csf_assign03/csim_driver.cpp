25d90480ac5bf883cd5efc9aeffb04daa2615acd
Liwen Tran <ltran29@jhu.edu>
2022-10-11 09:59:14 -0400

    Parsing input

--- csf_assign03/csim_driver.cpp
+++ csf_assign03/csim_driver.cpp
@@ -14,9 +14,26 @@ using std::endl;
 using namespace Csim;
 
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) { 
 
-    
+    // read each line of the trace file
+    string line;
+    while (getline(cin, line)) {
+        istringstream is(line);
+        
+        // read the first two fields (ignoring the third)
+        char process;       // either 'l' (loading) or 's' (storing)
+        string address_hex; // 32-bit memory address given in hexadecimal
+        int32_t address;    // 32-bit memory address in int
+
+        is >> process;
+        is >> address_hex;
+        address = stoi(address_hex, 0, 16);
+
+        // cout << "Process: " << process << ", Address: " << address_hex << ", Numeric address: " << address << endl;
+        
+        // Process line
+    }
 
     return 0;
 }
